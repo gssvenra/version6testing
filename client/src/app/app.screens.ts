67a1a6b3863bd2afe0faab161e56c6ff4a962019
Venugopal Ramamurthy <venugopal.ramamurthy@flex.com>
2018-05-06 21:35:00 -0700

    [Studio] Add screen "CategoryListView"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -3,11 +3,13 @@ import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu';
 import { Login_PhonePortrait } from '../pages/Login/PhonePortrait/Login';
 import { settings_PhonePortrait } from '../pages/settings/PhonePortrait/settings';
+import { categorylistview_PhonePortrait } from '../pages/categorylistview/PhonePortrait/categorylistview';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
     Login_PhonePortrait,
-    settings_PhonePortrait
+    settings_PhonePortrait,
+    categorylistview_PhonePortrait
   ];
   static mapping = {
     'Login': {
@@ -15,6 +17,9 @@ export class Screens {
     },
     'settings': {
       PhonePortrait: settings_PhonePortrait
+    },
+    'categorylistview': {
+      PhonePortrait: categorylistview_PhonePortrait
     }
   }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,3 +1,4 @@
+export * from './categorylistview/index';
 export * from './Login/index';
 export * from './menu/index';
 export * from './settings/index';
