546c0c31bce5345d71d9d34487b5265ea36b0942
Venugopal Ramamurthy <venugopal.ramamurthy@flex.com>
2018-05-07 02:01:59 -0700

    [Studio] Update source of file "server\client/Login.js"

--- server/client/Login.js
+++ server/client/Login.js
@@ -1,4 +1,4 @@
 exports.submit = async (session, models, vars) => {
-    await session.transform.version6testing.update('Login', models.Login);
-    await session.transform.version6testing.action('Login', 'submit');
+    await session.transform.version6testing.update('login', models.Login);
+    await session.transform.version6testing.action('login', 'submit');
 };
