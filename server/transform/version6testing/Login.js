068dd45646f0d612d0b53c39354b268aa4349fc3
Venugopal Ramamurthy <venugopal.ramamurthy@flex.com>
2018-05-06 22:02:11 -0700

    [Webexplorer] Connect screens to page model "Login", Delete connection from page model "Login"

--- server/transform/version6testing/Login.js
+++ server/transform/version6testing/Login.js
@@ -1,3 +1,7 @@
 exports.submit = async (session, models, vars) => {
     await session.transform.version6testing.action('Login', 'submit');
 };
+exports.Login = async (session, models, vars) => {
+    models.Login = vars.page;
+    await session.screen('Login');
+};
